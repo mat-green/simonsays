75944a9424136c426064f119e137cd5225069812
Matthew Green <matthew@newedgeengineering.com>
2015-10-19 00:12:25 +0100

    WIP

--- build.config.js
+++ build.config.js
@@ -7,8 +7,8 @@ module.exports = {
    * development and the `compile_dir` folder is where our app resides once it's
    * completely built.
    */
-  build_dir: 'build',
-  compile_dir: 'bin',
+  build_dir: 'build/www',
+  compile_dir: 'build/dist',
 
   /**
    * This is a collection of file patterns that refer to our app code (the
@@ -20,16 +20,16 @@ module.exports = {
    * app's unit tests.
    */
   app_files: {
-    js: [ 'src/**/*.js', '!src/**/*.spec.js', '!src/assets/**/*.js' ],
-    jsunit: [ 'src/**/*.spec.js' ],
+    js: [ 'app/**/*.js', '!**/*.spec.js', '!www/**/*.js', '!grunt/**/*.js' ],
+    jsunit: [ 'app/**/*.spec.js', '!grunt/**/*.tpl.js' ],
     
-    coffee: [ 'src/**/*.coffee', '!src/**/*.spec.coffee' ],
-    coffeeunit: [ 'src/**/*.spec.coffee' ],
+    coffee: [ 'app/**/*.coffee', '!app/**/*.spec.coffee' ],
+    coffeeunit: [ 'app/**/*.spec.coffee' ],
 
     atpl: [ 'src/app/**/*.tpl.html' ],
     ctpl: [ 'src/common/**/*.tpl.html' ],
 
-    html: [ 'src/index.html' ],
+    html: [ 'src/grunt/index.html' ],
     less: 'src/less/main.less'
   },
 
@@ -64,13 +64,12 @@ module.exports = {
     js: [
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-      'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js'
     ],
     css: [
     ],
     assets: [
     ]
-  },
+  }
 };
