e4af826ae5b136d6cafd8de0c8f7e74fcd2d6244
Matthew Green <matthew@newedgeengineering.com>
2015-10-20 23:48:56 +0100

    added cli version

--- grunt/config/pkg.js
+++ grunt/config/pkg.js
@@ -5,4 +5,10 @@ module.exports = function (grunt) {
      * version. It's already there, so we don't repeat ourselves here.
      */
     grunt.config.set('pkg', grunt.file.readJSON("package.json"));
+    var target = grunt.option('version') || grunt.template.today('yy.mm') + '.0';
+    grunt.config.merge({
+        pkg: {
+            version: target
+        }
+    });
 };
