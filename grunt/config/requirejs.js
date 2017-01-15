dafa9b1c114134f6d6c5055f481dcf30e40ed483
Matthew Green <matthew@newedgeengineering.com>
2017-01-15 19:17:13 +0000

    initial preview process

--- grunt/config/requirejs.js
+++ grunt/config/requirejs.js
@@ -1,41 +1,46 @@
 module.exports = function (grunt) {
 
     grunt.config.set('requirejs', {
-      compile: {
-        options: {
-          appDir: '<%= files.develop_dir %>',
-          mainConfigFile: '<%= files.develop_dir %>/config.js',
-          optimize: 'uglify2',
-          uglify2: {
-            'screw-ie8': true,
-            compress: {
-              global_defs: {
-                DEBUG: false
-              }
-            },
-            warnings: false,
-            // Safe here, probably due to implicit declarations in r.js
-            mangle: true
-          },
-          paths: {
-              'angular': 'empty:',
-              'angular-ui-router': 'empty:',
-              'angular-ui-bootstrap': 'empty:'
-          },
-          modules: [
-              {
-                  name: 'app/app.main',
-                  exclude: ['config']
-              }, {
-                  name: 'config'
-              }
-          ],
-          // Timeout explicitly disabled to ensure we do not get require.js timeout errors.
-          waitSeconds: 0,
-          skipDirOptimize: true,
-          removeCombined: true,
-          preserveLicenseComments: true,
-        //   dir: '<%= files.compile_dir %>'
+        compile: {
+            options: {
+                paths: {
+                //   'appFiles': './<%= files.develop_dir %>/src/app',
+                  'angular': 'empty:',
+                  'angular-ui-router': 'empty:',
+                  'angular-ui-bootstrap': 'empty:',
+                  'placeholders': 'lib/angular-placeholders/dist/placeholders-0.0.1-SNAPSHOT'
+                },
+                removeCombined: true,
+                dir: './<%= files.compile_dir %>/',
+                // name: 'app/app.module',
+                baseUrl: '<%= files.develop_dir %>/',
+        //   mainConfigFile: '<%= files.develop_dir %>/config.js',
+                optimize: 'uglify2',
+                uglify2: {
+        //     'screw-ie8': true,
+        //     compress: {
+        //       global_defs: {
+        //         DEBUG: false
+        //       }
+        //     },
+        //     warnings: false,
+        //     // Safe here, probably due to implicit declarations in r.js
+                    mangle: true
+                }
+        //   modules: [
+        //       {
+        //           name: 'app/app.main',
+        //           exclude: ['config']
+        //       }, {
+        //           name: 'config'
+        //       }
+        //   ],
+        //   // Timeout explicitly disabled to ensure we do not get require.js timeout errors.
+        //   waitSeconds: 0,
+        //   skipDirOptimize: true,
+        //   removeCombined: true,
+        //   preserveLicenseComments: true,
+        //   dir: '<%= files.compile_dir %>/app'
         }
       }
     });
--- src/common/placeholders/placeholders.module.js
+++
@@ -1,1 +0,0 @@
-angular.module("placeholders", ["placeholders.img","placeholders.txt"]);
